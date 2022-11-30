945a71fbbb45eca503ac0ab7c7444b48c53bf8cc
drzobin <robin.larsson@protonmail.ch>
2022-11-30 14:00:57 +0100

    Adding support for multiple seedfiles

--- elna.c
+++ elna.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <dirent.h>
 
 /*
 Run a command, kill the process if it hasent exit after sleep time.
@@ -26,12 +27,7 @@ int run_cmd(char *cmd,char *argv[], int run_time)
         // This is run by the child process.
         int exit_code;
         
-        //printf("cmd: %s\n",cmd);
-        //printf("argv[0]: %s\n",argv[0]);
-        //printf("argv[1]: %s\n",argv[1]);
         exit_code = execvp(cmd,argv);
-        // If execvp returns, it must have failed.
-        //exit(0);
         printf("execvp exit_code:%d\n",exit_code);
         // This is done by the child process.
         exit(exit_code);
@@ -65,7 +61,6 @@ void create_seedfile(char *originalFile,size_t size,int pos,char *value,char *ne
     ptr_fp = fopen(newFile,"a");
 
     //write to file
-    //fwrite(originalFile,size,1,ptr_fp);    
     for(int i = 0;i < size; i++){
         if(i == pos){
             fwrite(value,1,1,ptr_fp); 
@@ -74,8 +69,6 @@ void create_seedfile(char *originalFile,size_t size,int pos,char *value,char *ne
         fwrite(originalFile+i,1,1,ptr_fp); 
     }
     fclose(ptr_fp);    
-
-    //printf("hex: %X\n",originalFile[0]);
 }
 
 int main(int argc, char *argv[]){
@@ -89,10 +82,7 @@ int main(int argc, char *argv[]){
     cmd_argv[2] = NULL;
 
     // Full path to folder that contains seedfiles.
-    char seedfile_folder = "/home/drz/github/elna/";
-    
-    // Full path to original seed file.
-    char file[] = "/home/drz/github/elna/test.txt";
+    char seedfile_folder[] = "/home/drz/github/elna/seedfiles/";
 
     // Full path to tmp fuzzy file.
     char tmp_file[] = "/home/drz/github/elna/working_dir/test.txt";
@@ -111,78 +101,108 @@ int main(int argc, char *argv[]){
     
     // If this is set to 1, all bits in input file will be flipped.
     int flipp_all_bits = 0;
-    
-    // File size of original seed file.
-    size_t file_size = get_filesize(file);
-    printf("File size is: %zu\n",file_size);
-
-    // Read original file data to heap.
-    original_filedata = read_file(file,file_size);
-
-    // Start with this hex value.
-    char value = 0x00;
-    char *v_ptr = &value;
 
     // Start fuzzing.
-    int run_loop = 1;
-    // This loop is run on all offsets in seedfile.
-    while(run_loop == 1){
-        // If pos is -1 then we should flipp all bits in input file.
-        if(pos == -1){
-            flipp_all_bits = 1;
-            pos = 0;
+
+    // Open seedfile folder.
+    struct dirent *files;
+    DIR *dir = opendir(seedfile_folder);
+    if (dir == NULL){
+        printf("Directory cannot be opened!" );
+        return 0;
+    }
+
+    // This loop is run on all the seedfiles, one iteration per file.
+    while ((files = readdir(dir)) != NULL){
+        
+        // Name of seedfile.
+        char *seedfile_name = files->d_name;
+        
+        // The . and .. is not a files.
+        if ((strcmp(seedfile_name,".") == 0) || (strcmp(seedfile_name,"..") == 0)){
+            continue;
         }
-        // Bitflipp the offset value.
-        for(int i=0; i<255; i++){
-            printf("\n\n");
-            value++;
-            printf("Value: %hhx\n",value);
+        
+        // Filename with full path of seedfile.
+        int file_path_length  = strlen(seedfile_folder) + strlen(seedfile_name) + 1;
+        char file[file_path_length];
+        snprintf(file,file_path_length,"%s%s",seedfile_folder,seedfile_name);
 
-            // Create file to use as input.
-            create_seedfile(original_filedata,file_size,pos,v_ptr,tmp_file);
+        // Seedfile size.
+        size_t file_size = get_filesize(file);
 
-            // Run executable with newly created input.
-            int status = run_cmd(cmd,cmd_argv,wait_pid);
+        printf("Working with seedfile: %s\n", files->d_name);
+        printf("File size is: %zu\n",file_size);
 
-            // Executable did not crash, remove input file.
-            if(status == 0) {
-                int remove_status = remove(tmp_file);
-                if(remove_status != 0) {
-                    printf("Error removing tmp file\n");
-                }
-                printf("Exit code 0\n");
+        // Read original seedfile data to heap.
+        original_filedata = read_file(file,file_size);
+        
+         // This loop is run on all offsets in seedfile, one iteration per offset in seedfile.
+        int run_loop = 1;
+        while(run_loop == 1){
+            // If pos is -1 then we should flipp all bits in input file.
+            if(pos == -1){
+                flipp_all_bits = 1;
+                pos = 0;
             }
-            // Executable probably crashed, save input file. 
-            else {
-                printf("Exit code:%d\n",status);
-                char new_filename[64];
-                //sprintf(new_filename,"pos:%d_value:%hhx",pos,value);
-                snprintf(new_filename,63,"offset:%d_value:0x%hhx",pos,value);
-                
-                char out_file[sizeof(out_dir) + sizeof(new_filename)];
-                strcpy(out_file,out_dir);
-                strcat(out_file,new_filename);
 
-                int rename_status = rename(tmp_file,out_file);
-                printf("tmpFile: %s\n",tmp_file);
-                printf("outFile: %s\n",out_file);
-                if(rename_status != 0) {
-                    printf("Error moving tmp file, exiting\n");
-                    return 1;
+            // Start with this hex value.
+            char value = 0x00;
+            char *v_ptr = &value;
+            
+            // This loop is run on all the hex values, every iteration is one hex value.
+            for(int i=0; i<255; i++){
+                printf("\n\n");
+                value++;
+                printf("Value: %hhx\n",value);
+
+                // Create file to use as input.
+                create_seedfile(original_filedata,file_size,pos,v_ptr,tmp_file);
+
+                // Run executable with newly created input.
+                int status = run_cmd(cmd,cmd_argv,wait_pid);
+
+                // Executable did not crash, remove input file.
+                if(status == 0) {
+                    int remove_status = remove(tmp_file);
+                    if(remove_status != 0) {
+                        printf("Error removing tmp file\n");
+                    }
+                    printf("Exit code 0\n");
                 }
-            } 
-        }
-        //If we should only bit flipp a specific offset then only run loop one time.
-        if(flipp_all_bits == 0){
-            run_loop = 0;
-        // If we should bit flipp all bits in input file.
-        } else {
-            pos++;
-            if(pos > file_size){
+                // Executable probably crashed, save input file. 
+                else {
+                    printf("Exit code:%d\n",status);
+                    char new_filename[128];
+                    //sprintf(new_filename,"pos:%d_value:%hhx",pos,value);
+                    snprintf(new_filename,127,"filename:%s_offset:%d_value:0x%hhx",seedfile_name,pos,value);
+                
+                    char out_file[sizeof(out_dir) + sizeof(new_filename)];
+                    strcpy(out_file,out_dir);
+                    strcat(out_file,new_filename);
+
+                    int rename_status = rename(tmp_file,out_file);
+                    printf("tmpFile: %s\n",tmp_file);
+                    printf("outFile: %s\n",out_file);
+                    if(rename_status != 0) {
+                        printf("Error moving tmp file, exiting\n");
+                        return 1;
+                    }
+                } 
+            }
+            //If we should only bit flipp a specific offset then only run loop one time.
+            if(flipp_all_bits == 0){
                 run_loop = 0;
+            // If we should bit flipp all bits in input file.
+            } else {
+                pos++;
+                if(pos > file_size){
+                    run_loop = 0;
+                }
             }
         }
     }
+    closedir(dir);
 
     return 0;
 }
